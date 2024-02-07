d63ad4746bdc9af0b507c469cbec7d8e1a847f75
VishnuNair3132 <121325874+VishnuNair3132@users.noreply.github.com>
2024-02-07 11:21:48 +0530

    first commit

---
+++ ai-summerizer/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Hero from './Components/Hero'
+import Demo from './Components/Demo'
+
+function App() {
+
+  return (
+    <main>
+    <div className='main'>
+    <div className='gradient'/>
+
+    </div>
+    <div className='app'>
+    <Hero/>
+    <Demo/>
+
+    </div>
+
+    </main>
+  )
+}
+
+export default App
